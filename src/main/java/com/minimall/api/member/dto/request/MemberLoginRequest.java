e7f3aaf661c9fcc8fa38dace4915a9854fa63a2b
Victor_Cha <cts5109@gmail.com>
2026-01-07 12:15:28 +0900

    chore(member):remove-login-dtos

--- src/main/java/com/minimall/api/member/dto/request/MemberLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.minimall.api.member.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberLoginRequest(
-        @NotBlank String loginId,
-        @NotBlank String password
-) {
-}
